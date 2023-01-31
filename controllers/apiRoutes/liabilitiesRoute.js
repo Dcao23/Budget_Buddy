bea90966be8dd8cafea91789e09e0971f6a12dad
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-01-30 20:39:57 -0500

    added index files to the routes

--- controllers/apiRoutes/liabilitiesRoute.js
+++ controllers/apiRoutes/liabilitiesRoute.js
@@ -1,42 +1,42 @@
 const router = require('express').Router();
 const { Liabilities } = require('../models');
 
-router.get('/Liabilities:auto_loan', async (req, res) => {
+router.get('/auto_loan', async (req, res) => {
     const auto_loan = await auto_loan.findAll().catch((err) => {
         res.json(err);
     });
     res.json(auto_loan);
 });
 
-router.get('/Liabilities:mortgage_rent', async (req, res) => {
+router.get('/mortgage_rent', async (req, res) => {
     const personal_loan = await personal_loan.findAll().catch((err) => {
         res.json(err);
     });
     res.json(personal_loan);
 });
 
-router.get('/Liabilities:auto_loan', async (req, res) => {
+router.get('/auto_loan', async (req, res) => {
     const mortgage_rent = await mortgage_rent.findAll().catch((err) => {
         res.json(err);
     });
     res.json(mortgage_rent);
 });
 
-router.get('/Liabilities:credit_card', async (req, res) => {
+router.get('/credit_card', async (req, res) => {
     const credit_card = await credit_card.findAll().catch((err) => {
         res.json(err);
     });
     res.json(credit_card);
 });
 
-router.get('/Liabilities:student_loan', async (req, res) => {
+router.get('/student_loan', async (req, res) => {
     const student_loan = await student_loan.findAll().catch((err) => {
         res.json(err);
     });
     res.json(student_loan);
 });
 
-router.get('/Liabilities:utilities', async (req, res) => {
+router.get('/utilities', async (req, res) => {
     const utilities = await utilities.findAll().catch((err) => {
         res.json(err);
     });
