17f7589e7d4cfeee8bdd71ec780270e213e684aa
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-02-02 18:47:31 -0500

    updated seed files

--- controllers/apiRoutes/assetsRoute.js
+++ controllers/apiRoutes/assetsRoute.js
@@ -1,35 +1,46 @@
 const router = require('express').Router();
 const { Asset } = require('../apiRoutes')
 
+router.get('/', async (req, res) => {
+  try {
+    //to do need to code this out
+    console.log('hello')
+  }
+  catch {
+    // to do need to code this out
+    console.log('error')
+  }
+})
+
 router.post('/', async (req, res) => {
-    try {
-      const newAsset = await Asset.create({
-        ...req.body,
+  try {
+    const newAsset = await Asset.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newAsset);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const assetData = await Asset.destroy({
+      where: {
+        id: req.params.id,
         user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newAsset);
-    } catch (err) {
-      res.status(400).json(err);
+      },
+    });
+    if (!assetData) {
+      res.status(404).json({ message: 'Required input field' });
+      return;
     }
-  });
-  
-  router.delete('/:id', async (req, res) => {
-    try {
-      const assetData = await Asset.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-      if (!assetData) {
-        res.status(404).json({ message: 'Required input field' });
-        return;
-      }
-      res.status(200).json(assetData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  module.exports = router;
+    res.status(200).json(assetData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -6,7 +6,7 @@ const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     await seedLiabilities();
-    console.log('\n----- CATEGORIES SEEDED -----\n');
+    console.log('\n----- LIABILITIES SEEDED -----\n');
 
     process.exit(0);
 };
