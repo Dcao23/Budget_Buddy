a70d729cd2e01551c5c09c04e05ecadb2c80561d
Russ Cherry <114961971+RussC22@users.noreply.github.com>
2023-02-06 01:01:03 -0500

    Update userRoute.js

--- controllers/apiRoutes/userRoute.js
+++ controllers/apiRoutes/userRoute.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-const User = require('../../models/User');
-
-
- router.get('/', async (req, res) => {
-    try {
-        const userData = await User.findAll();
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-=======
 const router = require("express").Router();
 const User = require("../../models/User");
 
@@ -21,7 +8,6 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
->>>>>>> c0cc583c5dac1fe03a766f2879854934eead7a45
 });
 
 router.post("/", async (req, res) => {
